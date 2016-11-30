a127cdcadcbd9a36c4621d65d1959a7aebe9b222
g-sato <g-sato@logizard.jp>
2016-11-30 11:21:48 +0900

    add restapi

--- lib/embulk/input/restapi.rb
+++ lib/embulk/input/restapi.rb
@@ -5,19 +5,18 @@ module Embulk
       Plugin.register_input("restapi", self)
 
       def self.transaction(config, &control)
-        # configuration code:
         task = {
-          "option1" => config.param("option1", :integer),                     # integer, required
-          "option2" => config.param("option2", :string, default: "myvalue"),  # string, optional
-          "option3" => config.param("option3", :string, default: nil),        # string, optional
+          'base_url' => config.param('base_url', :string, default: nil),
+          'path' => config.param('path', :string, default: nil),
+          'json_root' => config.param('json_root', :string, default: "$"),
+          'method' => config.param('method', :string, default: 'get'),
+          'headers' => config.param('headers', :array, default: []),
+          'params' => config.param('params', :array, default: []),
+          'columns' => config.param('columns', :array, default: []),
         }
-
-        columns = [
-          Column.new(0, "example", :string),
-          Column.new(1, "column", :long),
-          Column.new(2, "value", :double),
-        ]
-
+        columns = task['columns'].each_with_index.map do |c, i|
+          Column.new(i, c["name"], c["type"].to_sym)
+        end
         resume(task, columns, 1, &control)
       end
 
@@ -28,30 +27,70 @@ module Embulk
         return next_config_diff
       end
 
-      # TODO
-      #def self.guess(config)
-      #  sample_records = [
-      #    {"example"=>"a", "column"=>1, "value"=>0.1},
-      #    {"example"=>"a", "column"=>2, "value"=>0.2},
-      #  ]
-      #  columns = Guess::SchemaGuess.from_hash_records(sample_records)
-      #  return {"columns" => columns}
-      #end
-
-      def init
-        # initialization code:
-        @option1 = task["option1"]
-        @option2 = task["option2"]
-        @option3 = task["option3"]
-      end
-
       def run
-        page_builder.add(["example-value", 1, 0.1])
-        page_builder.add(["example-value", 2, 0.2])
-        page_builder.finish
-
+        response = request
+        find_by_path(response, @task["json_root"]).each do |order|
+          @page_builder.add(make_record(@task["columns"], order))
+        end
+        @page_builder.finish
         task_report = {}
         return task_report
+      end
+
+      def make_record(columns, js_resource)
+        columns.map do |column|
+          name = column["name"]
+          path = column["path"]
+          val = path.nil? ? js_resource[name] : find_by_path(js_resource, path)
+
+          v = val.nil? ? "" : val
+          type = column["type"]
+          case type
+            when "string"
+              v
+            when "long"
+              v.to_i
+            when "double"
+              v.to_f
+            when "boolean"
+              if v.nil?
+                nil
+              elsif v.kind_of?(String)
+                ["yes", "true", "1"].include?(v.downcase)
+              elsif v.kind_of?(Numeric)
+                !v.zero?
+              else
+                !!v
+              end
+            when "timestamp"
+              v.empty? ? nil : Time.iso8601(v)
+            else
+              raise "Unsupported type #{type}"
+          end
+        end
+      end
+
+      def find_by_path(e, path)
+        JsonPath.on(e, path).first
+      end
+
+      def request
+        conn = Faraday.new(:url => @task[:base_url]) do |builder|
+          builder.request  :url_encoded
+          builder.adapter  :net_http
+        end
+
+        response = conn.get do |req|
+          req.url @task[:path]
+          req.headers['Content-Type'] = 'application/json'
+          @task[:headers].each do |head|
+            req.headers[head["name"]] = head["value"]
+          end
+          @task[:params].each do |param|
+            req.params[param["name"]] = param["value"]
+          end
+        end
+        response.body
       end
     end
 
